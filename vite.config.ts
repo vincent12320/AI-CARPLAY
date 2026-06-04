7962861d70096e68ca0caaae961f4f0cc6624d90
vincentli <111236192+vincent12320@users.noreply.github.com>
2026-06-04 11:08:02 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     server: { allowedHosts: true },
+    plugins: []
   },
   tanstackStart: {
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
